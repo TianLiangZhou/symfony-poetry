754fbb2268b718dfb75d90b66cbe5c0693167da9
zhoutianliang <mfkgdyve@gmail.com>
2023-04-17 11:06:31 +0800

    fix mobile

--- templates/kutty/dev/js/kutty.js
+++ templates/kutty/dev/js/kutty.js
@@ -1,6 +1,44 @@
 import "alpinejs/dist/cdn.min"
 
 (function (root) {
+  var cookie= {
+    getItem: function (sKey) {
+      return decodeURIComponent(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent(sKey).replace(/[-.+*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null;
+    },
+    setItem: function (sKey, sValue, vEnd, sPath, sDomain, bSecure) {
+      if (!sKey || /^(?:expires|max\-age|path|domain|secure)$/i.test(sKey)) { return false; }
+      var sExpires = "";
+      if (vEnd) {
+        switch (vEnd.constructor) {
+          case Number:
+            sExpires = vEnd === Infinity ? "; expires=Fri, 31 Dec 9999 23:59:59 GMT" : "; max-age=" + vEnd;
+            break;
+          case String:
+            sExpires = "; expires=" + vEnd;
+            break;
+          case Date:
+            sExpires = "; expires=" + vEnd.toUTCString();
+            break;
+        }
+      }
+      document.cookie = encodeURIComponent(sKey) + "=" + encodeURIComponent(sValue) + sExpires + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "") + (bSecure ? "; secure" : "");
+      return true;
+    },
+    removeItem: function (sKey, sPath, sDomain) {
+      if (!sKey || !this.hasItem(sKey)) { return false; }
+      document.cookie = encodeURIComponent(sKey) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" + ( sDomain ? "; domain=" + sDomain : "") + ( sPath ? "; path=" + sPath : "");
+      return true;
+    },
+    hasItem: function (sKey) {
+      return (new RegExp("(?:^|;\\s*)" + encodeURIComponent(sKey).replace(/[-.+*]/g, "\\$&") + "\\s*\\=")).test(document.cookie);
+    },
+    keys: /* optional method: you can safely remove it! */ function () {
+      var aKeys = document.cookie.replace(/((?:^|\s*;)[^\=]+)(?=;|$)|^\s*|\s*(?:\=[^;]*)?(?:\1|$)/g, "").split(/\s*(?:\=[^;]*)?;\s*/);
+      for (var nIdx = 0; nIdx < aKeys.length; nIdx++) { aKeys[nIdx] = decodeURIComponent(aKeys[nIdx]); }
+      return aKeys;
+    }
+  };
+
   root.Components = {}, root.Components.popover = function ({
                                                               open: e = !1,
                                                               focus: t = !1
@@ -58,9 +96,10 @@ import "alpinejs/dist/cdn.min"
   ;
 
   document.addEventListener('alpine:init', () => {
+    let _theme = cookie.getItem('_theme');
     Alpine.store('theme', {
-      current: 'System',
-      system: window.matchMedia('(prefers-color-scheme: dark)').matches ? 'Dark' : 'Light',
+      current: _theme || 'System',
+      system: (_theme || 'System') == 'System' ? (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'Dark' : 'Light') : _theme,
       toggle(theme) {
         this.current = theme;
         if (theme == 'System') {
@@ -68,6 +107,7 @@ import "alpinejs/dist/cdn.min"
         } else {
           this.system = theme;
         }
+        cookie.setItem('_theme', theme);
       }
     });
   });
