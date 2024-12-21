132ff81d964ff8a9e38956120ee1aae85322f478
zhoutianliang <mfkgdyve@gmail.com>
2024-12-22 00:30:42 +0800

    use ckfinder-symfony-bundle

--- config/packages/validator.php
+++ config/packages/validator.php
@@ -5,5 +5,5 @@ use Symfony\Config\FrameworkConfig;
 return static function (FrameworkConfig $config) {
     $config->validation()
         ->emailValidationMode('html5')
-        ->enableAnnotations(true);
+    ;
 };
